26104e054abfe11b61039c16adc9e135fd133d1c
Hrishikesh Bhatt <96074695+Hrishi4kesh@users.noreply.github.com>
2023-05-07 02:12:56 +0530

    Home page and exit confirmation

---
+++ app/src/main/java/com/example/fitify/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.fitify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int TIME_INTERVAL = 2000;
+    private long backPressed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(backPressed + TIME_INTERVAL > System.currentTimeMillis()) {
+            super.onBackPressed();
+            finish();
+            return;
+        }
+        else{
+            Toast.makeText(getBaseContext(),"Press Back Again To Exit",Toast.LENGTH_SHORT).show();
+        }
+        backPressed = System.currentTimeMillis();
+    }
+}
