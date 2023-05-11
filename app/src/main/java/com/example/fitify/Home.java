8e8feab0965aec8a7f66ebe1cab9cf5263d09d9e
Hrishikesh Bhatt <96074695+Hrishi4kesh@users.noreply.github.com>
2023-05-11 23:50:30 +0530

    Home configured

--- app/src/main/java/com/example/fitify/Home.java
+++ app/src/main/java/com/example/fitify/Home.java
@@ -4,18 +4,60 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.Observable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.result.DailyTotalResult;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.util.concurrent.Flow;
 import java.util.concurrent.TimeUnit;
 
+import kotlin.jvm.internal.Intrinsics;
+
 public class Home extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
-}
+    public View onCreateView(@NotNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Intrinsics.checkNotNullParameter(inflater, "inflater");
+        View rootView = inflater.inflate(R.layout.activity_home, container, false);
+        View textViewSteps=rootView.findViewById(R.id.steps);
+        Intrinsics.checkNotNullExpressionValue(textViewSteps, "rootView.findViewById(R.id.steps)");
+
+        View textViewStepsBig=rootView.findViewById(R.id.steps_big);
+        Intrinsics.checkNotNullExpressionValue(textViewStepsBig, "rootView.findViewById(R.id.steps_big)");
+
+        View textViewCalories=rootView.findViewById(R.id.burned_calories);
+        Intrinsics.checkNotNullExpressionValue(textViewCalories, "rootView.findViewById(R.id.burned_calories)");
+
+        View textViewDistance=rootView.findViewById(R.id.distance);
+        Intrinsics.checkNotNullExpressionValue(textViewDistance, "rootView.findViewById(R.id.distance)");
+
+        View stepsProgressBar=rootView.findViewById(R.id.stepsProgressBar);
+        Intrinsics.checkNotNullExpressionValue(stepsProgressBar, "rootView.findViewById(R.id.stepsProgressBar)");
+        ProgressBar progressBar = findViewById(R.id.stepsProgressBar);
+        if (progressBar == null) {
+            Intrinsics.throwUninitializedPropertyAccessException("stepsProgressBar");
+        }
+//        FitnessViewModel var6 = this.getFitnessViewModel();
+//        Intrinsics.checkNotNullExpressionValue(rootView, "rootView");
+//        Context var10002 = rootView.getContext();
+//        Intrinsics.checkNotNullExpressionValue(var10002, "rootView.context");
+//        var10000.setMax(var6.loadObjectiveSteps(var10002));
+//        FitnessViewModel var5 = this.getFitnessViewModel();
+//        Context var7 = rootView.getContext();
+//        Intrinsics.checkNotNullExpressionValue(var7, "rootView.context");
+//        var5.getDailyFitnessData(var7).observe(this.getViewLifecycleOwner(), (Observer)(new Observer() {
+        return rootView;
+    }}
--- app/src/main/java/com/example/fitify/MainActivity.java
+++ app/src/main/java/com/example/fitify/MainActivity.java
@@ -2,20 +2,43 @@ package com.example.fitify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptionsExtension;
+import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import kotlin.jvm.internal.Intrinsics;
+
 public class MainActivity extends AppCompatActivity {
     private static final int TIME_INTERVAL = 2000;
     private long backPressed;
+
+        private  int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 1;
+        private BottomNavigationView bottomNavigationView;
+        private  boolean runningQOrLater;
+        private  FitnessOptions fitnessOptions;
+        private final String TAG="MainActivity";
+
+
     
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GoogleSignInAccount getGoogleAccount;
 
+    }
+    private GoogleSignInAccount getGoogleAccount() {
+        GoogleSignInAccount var10000 = GoogleSignIn.getAccountForExtension((Context)this, (GoogleSignInOptionsExtension)this.fitnessOptions);
+        Intrinsics.checkNotNullExpressionValue(var10000, "GoogleSignIn.getAccountF…ion(this, fitnessOptions)");
+        return var10000;
     }
 
 
