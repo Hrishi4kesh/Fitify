ec4ef0fc399e8b9de04bfbf7449d06ad0f4ee3a7
Hrishikesh Bhatt <96074695+Hrishi4kesh@users.noreply.github.com>
2023-05-16 15:26:10 +0530

    WeeklyFitnessModel Added

---
+++ app/src/main/java/com/example/fitify/WeeklyFitnessModel.java
@@ -0,0 +1,11 @@
+package com.example.fitify;
+
+import java.util.List;
+
+public class WeeklyFitnessModel {
+    private List dailyFitnessList;
+
+    public List getDailyFitnessList() {
+        return this.dailyFitnessList;
+    }
+}
