69495548e6902f502a5c1852cc92c1e66bfb03f9
shytortoise19 <ishas8799@gmail.com>
2023-05-17 00:00:52 +0530

    last week data

--- app/src/main/java/com/example/fitify/Statistics.java
+++ app/src/main/java/com/example/fitify/Statistics.java
@@ -4,46 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.google.android.gms.fitness.data.DataSet;
-import com.google.android.gms.fitness.data.DataType;
-import com.google.android.gms.fitness.request.DataReadRequest;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class Statistics extends AppCompatActivity {
 
-    GraphView graphView;
-    data_retrieve data=new data_retrieve();
-    List<DataSet> lastSevenDaysDatasets = data.getLastSevenDaysDatasets();
+    private GraphView graphView;
+    private data_retrieve data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_statistics);
+
+        // Initialize data_retrieve instance
+        data = new data_retrieve(this);
+
+        // Get last seven days steps walked
+        List<Integer> lastSevenDaysDatasets = data.getStepsWalkedLastSevenDays();
+
         graphView = findViewById(R.id.idGraphView);
-        List<DataPoint> dataPoints = new ArrayList<>();
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
+
+        // Add data points to the series
         for (int i = 0; i < lastSevenDaysDatasets.size(); i++) {
-            DataSet dataset = lastSevenDaysDatasets.get(i);
-            int steps = 0;
-            DataPoint dataPoint = new DataPoint((double) i, steps);
-           dataPoints.add(dataPoint);
+            double x = i;
+            double y = lastSevenDaysDatasets.get(i);
+            DataPoint dataPoint = new DataPoint(x, y);
+            series.appendData(dataPoint, true, lastSevenDaysDatasets.size());
         }
-        DataPoint[] dataPointArray = dataPoints.toArray(new DataPoint[0]);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(dataPointArray);
-
-
-
         graphView.setTitle("Steps walked");
         graphView.setTitleColor(R.color.purple_200);
         graphView.setTitleTextSize(18);
         graphView.addSeries(series);
     }
 }
-
-
-//setContentView(R.layout.fragment_statistics);
--- app/src/main/java/com/example/fitify/data_retrieve.java
+++ app/src/main/java/com/example/fitify/data_retrieve.java
@@ -1,17 +1,15 @@
 package com.example.fitify;
-import static android.content.ContentValues.TAG;
+
+import android.content.Context;
 import android.util.Log;
+
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.fitness.Fitness;
-import com.google.android.gms.fitness.FitnessOptions;
 import com.google.android.gms.fitness.data.Bucket;
 import com.google.android.gms.fitness.data.DataSet;
 import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
 import com.google.android.gms.fitness.request.DataReadRequest;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -19,87 +17,54 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class data_retrieve {
-    ZonedDateTime endTime = LocalDateTime.now().atZone(ZoneId.systemDefault());
-    ZonedDateTime startTime = endTime.minusWeeks(1);
-    private List<DataSet> lastSevenDaysDatasets = new ArrayList<>();
+    private Context context;
 
-    private List<DataSet> getFitnessDataForLastSevenDays() {
-        Fitness.getHistoryClient(this, GoogleSignIn.getAccountForExtension(this, fitnessOptions))
-                .readData(createReadRequestForLastSevenDays())
-                .addOnSuccessListener(response -> {
-                    List<DataSet> datasetList = new ArrayList<>();
-                    for (Bucket bucket : response.getBuckets()) {
-                        for (DataSet dataSet : bucket.getDataSets()) {
-                            datasetList.addAll(dumpDataSet(dataSet));
-                        }
-                    }
-                    Calendar calendar = Calendar.getInstance();
-                    calendar.add(Calendar.DAY_OF_YEAR, -7);
-                    Date sevenDaysAgo = calendar.getTime();
-
-                    List<DataSet> lastSevenDaysDatasets = new ArrayList<>();
-                    for (DataSet dataset : datasetList) {
-                        long datasetEndTime = dataset.getEndTime(TimeUnit.MILLISECONDS);
-                        if (datasetEndTime >= sevenDaysAgo.getTime()) {
-                            lastSevenDaysDatasets.add(dataset);
-                        }
-                    }
-
-                    // Do further processing or return the list of datasets
-                    return lastSevenDaysDatasets;
-                })
-                .addOnFailureListener(e ->
-                        Log.w(TAG, "There was an error reading data from Google Fit", e));
-
-        // Return an empty list if the retrieval fails or hasn't finished yet
-        return new ArrayList<>();
+    public data_retrieve(Context context) {
+        this.context = context;
     }
 
-    private DataReadRequest createReadRequestForLastSevenDays() {
-        // Get the start and end times for the last seven days
+    public List<Integer> getStepsWalkedLastSevenDays() {
+        List<Integer> stepsList = new ArrayList<>();
+
+        // Set the start and end times for the last 7 days
         Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
         long endTime = calendar.getTimeInMillis();
         calendar.add(Calendar.DAY_OF_YEAR, -7);
         long startTime = calendar.getTimeInMillis();
 
-        // Create a DataReadRequest for the last seven days of fitness data
-        return new DataReadRequest.Builder()
-                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+        // Create a data read request to retrieve step count data for the specified time range
+        DataReadRequest readRequest = new DataReadRequest.Builder()
                 .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
                 .bucketByTime(1, TimeUnit.DAYS)
+                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
                 .build();
+
+        // Retrieve the step count data from Google Fit
+        Fitness.getHistoryClient(context, GoogleSignIn.getLastSignedInAccount(context))
+                .readData(readRequest)
+                .addOnSuccessListener(response -> {
+                    if (response.getBuckets().size() > 0) {
+                        // Iterate over the buckets and extract step count data
+                        for (Bucket bucket : response.getBuckets()) {
+                            List<DataSet> dataSets = bucket.getDataSets();
+                            for (DataSet dataSet : dataSets) {
+                                // Iterate over the data points in the dataset
+                                for (com.google.android.gms.fitness.data.DataPoint dataPoint : dataSet.getDataPoints()) {
+                                    for (Field field : dataPoint.getDataType().getFields()) {
+                                        if (field.getName().equals(Field.FIELD_STEPS.getName())) {
+                                            int steps = dataPoint.getValue(field).asInt();
+                                            stepsList.add(steps);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                })
+                .addOnFailureListener(e ->
+                        Log.e("data_retrieve", "Failed to retrieve step count data", e));
+
+        return stepsList;
     }
-
-    private List<DataSet> dumpDataSet(DataSet dataSet) {
-        List<DataSet> datasetList = new ArrayList<>();
-        datasetList.add(dataSet);
-        return datasetList;
-    }
-
-    private String getStartTimeString() {
-        return Instant.ofEpochSecond(this.getStartTime(TimeUnit.SECONDS))
-                .atZone(ZoneId.systemDefault())
-                .toLocalDateTime().toString();
-    }
-
-    private String getEndTimeString() {
-        return Instant.ofEpochSecond(this.getEndTime(TimeUnit.SECONDS))
-                .atZone(ZoneId.systemDefault())
-                .toLocalDateTime().toString();
-    }
-    public List<DataSet> getLastSevenDaysDatasets() {
-        return lastSevenDaysDatasets;
-    }
-    private long getEndTime(TimeUnit timeUnit) {
-        return endTime.toInstant().toEpochMilli();
-    }
-
-    private long getStartTime(TimeUnit timeUnit) {
-        return startTime.toInstant().toEpochMilli();
-    }
-
-
-
-
-
 }
