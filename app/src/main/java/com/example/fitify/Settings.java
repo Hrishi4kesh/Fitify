ffeaf0b47375c33f12e12ad1ea1e257c03f0ba05
Himanshi <shimanshi924@gmail.com>
2023-05-16 18:24:37 +0530

    settings config and xml

--- app/src/main/java/com/example/fitify/Settings.java
+++ app/src/main/java/com/example/fitify/Settings.java
@@ -11,38 +11,39 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-public class Settings extends AppCompatActivity {
+import kotlin.Lazy;
+
+
+public class Settings extends Fragment {
 
     private TextView stepsTextView;
     private Button changeObjButton;
     private FitnessViewModel fitnessViewModel;
 
+  /*  private final FitnessViewModel getFitnessViewModel(){
+        Lazy<FitnessViewModel> var1 = this.fitnessViewModelDelegate;
+        return (FitnessViewModel) var1.getValue();
+    }*/
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_settings);
         fitnessViewModel = new ViewModelProvider(this).get(FitnessViewModel.class);
     }
 
-   /* @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.activity_settings, container, false);
+   /* @Nullable shifted to last */
 
-        stepsTextView = rootView.findViewById(R.id.steps);
-        changeObjButton = rootView.findViewById(R.id.changeObj);
-
-        showObjectiveSteps(getContext());
-        changeObjButton.setOnClickListener(v -> showObjectiveDialog(requireContext()));
-
-        return rootView;
-    }*/
-
-    private void showObjectiveSteps(Context context) {
-        stepsTextView.setText(String.valueOf(fitnessViewModel.loadObjectiveSteps(context)));
+    private final void showObjectiveSteps(Context context) throws IllegalAccessException {
+        TextView stepsTextView = this.stepsTextView;
+        if (stepsTextView == null) {
+            throw new IllegalAccessException("stepsTextView must not be null");
+        }
+        // stepsTextView.setText(String.valueOf(this.getFitnessViewModel().leadObjectiveSteps(context)));
     }
+
 
     private void showObjectiveDialog(Context context) {
         View dialogView = LayoutInflater.from(context).inflate(R.layout.fragment_settings, null);
@@ -62,19 +63,75 @@ public class Settings extends AppCompatActivity {
         dialogBuilder.setPositiveButton("Save", (dialog, which) -> {
             int newObjectiveSteps = objectiveSeekBar.getValue() * 1000;
             fitnessViewModel.saveObjectiveSteps(context, newObjectiveSteps);
-            showObjectiveSteps(context);
+            try {
+                showObjectiveSteps(context);
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
             Toast.makeText(context, "Objective steps saved", Toast.LENGTH_SHORT).show();
         });
 
-
         dialogBuilder.create().show();
-    }
-   /* public settings(){
-        Function0 ownerProducer$iv = (Function0)(new settings());
-        Function0 extraProducer$iv = null;
-        Function0 factoryProducer$iv = null;
-        // int viewModels = false;
 
+
+    }
+
+
+    /* public Setting() {
+    Function0<SettingViewModel> ownerProducer$iv = new Function0<SettingViewModel>() {
+        @Override
+        public SettingViewModel invoke() {
+            return new SettingViewModel(Setting.this);
+        }
+    };
+    Function0<?> extrasProducer$iv = null;
+    Function0<?> factoryProducer$iv = null;
+    int $i$f$viewModels = false;
+    Lazy<SettingViewModel> owner$delegate$iv = LazyKt.lazy(LazyThreadSafetyMode.NONE, new Function0<SettingViewModel>() {
+        @Override
+        public SettingViewModel invoke() {
+            return new SettingViewModel(Setting.this);
+        }
+    });
+    this.fitnessViewModelDelegate = FragmentViewModelLazyKt.createViewModelLazy(
+            this,
+            Reflection.getOrCreateKotlinClass(FitnessViewModel.class),
+            new Function0<SettingViewModel>() {
+                @Override
+                public SettingViewModel invoke() {
+                    return (SettingViewModel) owner$delegate$iv.getValue();
+                }
+            },
+            new Function0<SettingViewModel>() {
+                @Override
+                public SettingViewModel invoke() {
+                    return (SettingViewModel) extrasProducer$iv.invoke();
+                }
+            },
+            new Function0<SettingViewModel>() {
+                @Override
+                public SettingViewModel invoke() {
+                    return new SettingViewModel(Setting.this);
+                }
+            }
+    );
+}*/
+
+
+
+
+     /* @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_settings, container, false);
+
+        stepsTextView = rootView.findViewById(R.id.steps);
+        changeObjButton = rootView.findViewById(R.id.changeObj);
+
+        showObjectiveSteps(getContext());
+        changeObjButton.setOnClickListener(v -> showObjectiveDialog(requireContext()));
+
+        return rootView;
     }*/
 }
 
